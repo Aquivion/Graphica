a2fe5e0195ef94729f4b04cac25e14d825fd1818
Aquivion <76964020+Aquivion@users.noreply.github.com>
2025-01-18 19:33:08 +0100

    add vulkan test and required configurations

--- main.cpp
+++ main.cpp
@@ -1,13 +1,35 @@
+#include <vulkan/vulkan.h>
 #include <iostream>
-using namespace std;
 
 int main()
 {
-    int x = 5;
+    // Vulkan instance creation info
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Vulkan Test";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
 
-    cout << "Hello world!" << endl;
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
 
-    cin.get();
+    // Create Vulkan instance
+    VkInstance instance;
+    VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
 
-    return 0;
+    if (result != VK_SUCCESS)
+    {
+        std::cerr << "Failed to create Vulkan instance!" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "Vulkan instance created successfully!" << std::endl;
+
+    // Clean up Vulkan instance
+    vkDestroyInstance(instance, nullptr);
+
+    return EXIT_SUCCESS;
 }
