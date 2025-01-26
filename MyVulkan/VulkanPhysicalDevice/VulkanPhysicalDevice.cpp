c86e8c8edb1aa314148b9eeabed7771c7e9d747c
Aquivion <76964020+Aquivion@users.noreply.github.com>
2025-01-26 14:52:22 +0100

    Add finding physical device functionality

--- MyVulkan/VulkanPhysicalDevice/VulkanPhysicalDevice.cpp
+++ MyVulkan/VulkanPhysicalDevice/VulkanPhysicalDevice.cpp
@@ -12,4 +12,50 @@ void VulkanPhysicalDevice::pickPhysicalDevice(VkInstance instance) {
     }
 
     std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+    for (const auto& device : devices) {
+        if (isDeviceSuitable(device)) {
+            physicalDevice = device;
+            break;
+        }
+    }
+
+    if (physicalDevice == VK_NULL_HANDLE) {
+        throw std::runtime_error("failed to find a suitable GPU!");
+    }
+}
+
+QueueFamilyIndices VulkanPhysicalDevice::findQueueFamilies(VkPhysicalDevice device) {
+    QueueFamilyIndices indices;
+    uint32_t queueFamilyCount = 0;
+
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+    int i = 0;
+    for (const auto& queueFamily : queueFamilies) {
+        if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            indices.graphicsFamily = i;
+        }
+        if (indices.isComplete()) {
+            break;
+        }
+        i++;
+    }
+
+    return indices;
+}
+
+bool VulkanPhysicalDevice::isDeviceSuitable(VkPhysicalDevice device) {
+    // Because Vulkan support of the device is all we need for now we just go with any device that
+    // is found. See https://shorturl.at/BKkJU in the Vulkan tutorial, on how to select devices that
+    // fullfill specifc features and properties that the application needs
+
+    QueueFamilyIndices indices = findQueueFamilies(device);
+
+    return indices.isComplete();
+    ;
 }
--- MyVulkan/VulkanPhysicalDevice/VulkanPhysicalDevice.h
+++ MyVulkan/VulkanPhysicalDevice/VulkanPhysicalDevice.h
@@ -2,6 +2,13 @@
 
 #include <vulkan/vulkan.h>
 
+#include <optional>
+
+struct QueueFamilyIndices {
+    std::optional<uint32_t> graphicsFamily;
+    bool isComplete() { return graphicsFamily.has_value(); }
+};
+
 class VulkanPhysicalDevice {
    public:
     VkPhysicalDevice getPhysicalDevice() { return physicalDevice; }
@@ -9,5 +16,16 @@ class VulkanPhysicalDevice {
    private:
     VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
     void pickPhysicalDevice(VkInstance instance);
+    /**
+     * @brief Checks if the physical device (graphics card) is suitable for operations that need to
+     * be performed
+     */
     bool isDeviceSuitable(VkPhysicalDevice device);
+    /**
+     * @brief Finds the supported queue families for the given device. Every command that is
+     * interacting with the graphics card needs to be submitted to a queue. There are different
+     * types of queues distinguished by their family. Every Queue family allows only a subset of
+     * commands
+     */
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
 };
