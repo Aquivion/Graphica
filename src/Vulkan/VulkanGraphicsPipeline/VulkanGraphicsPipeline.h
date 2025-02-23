7c017a7ab3e0bbe19a6606797232c8051c17b43d
Aquivion <76964020+Aquivion@users.noreply.github.com>
2025-02-23 23:35:59 +0100

    Setup fixed function graphic pipeline stages

--- src/Vulkan/VulkanGraphicsPipeline/VulkanGraphicsPipeline.h
+++ src/Vulkan/VulkanGraphicsPipeline/VulkanGraphicsPipeline.h
@@ -6,21 +6,25 @@
 namespace VulkanCore {
 
 class VulkanLogicalDevice;
+class VulkanSwapChain;
 
 class VulkanGraphicsPipeline {
    public:
     VulkanGraphicsPipeline();
 
-    void createGraphicsPipeline(VulkanLogicalDevice& logicalDevice);
+    void createGraphicsPipeline(const VulkanLogicalDevice& device, const VulkanSwapChain& swapChain);
 
     void populatePipelineShaderStageCreateInfo(VkPipelineShaderStageCreateInfo& createInfo,
                                                const VulkanShaderModule& shaderModule, VkShaderStageFlagBits stage);
 
     int getValue() const;
 
+    void cleanup(const VulkanLogicalDevice& device);
+
    private:
     VulkanShaderModule vertShaderModule;
     VulkanShaderModule fragShaderModule;
+    VkPipelineLayout pipelineLayout;
 };
 
 }  // namespace VulkanCore
